fc764e6132bbe91219b3c20ed53cdbbb534b1b2d
Ucpounds9191 <ucpounds9191@gmail.com>
2022-09-08 12:16:11 -0700

    entry point

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - My Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
