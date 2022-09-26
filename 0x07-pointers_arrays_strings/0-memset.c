105c5bd1bf60e0b56646fab4accd1270fabd535d
Ucpounds9191 <ucpounds9191@gmail.com>
2022-09-26 11:47:29 -0700

    set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,39 +1,18 @@
-#ifndef MAIN_H                                                                                                                     
-#define MAIN_H
-/**                                                                                                                               
- * File: main.h                                                                                                                   
- * Auth: sam tech (samuel birhanu)                                                                                                 
- * description: Header file containing prototypes for all functions                                                               
- * used in the 0x06-pointers_arrays_strings directory.                                                                       
-
- */                                                                                                                               
-
-                                                                                                                                   
-
-#include <stddef.h>                                                                                                               
-
-                                                                                                                                   
-
-int _putchar(char c);                                                                                                              
-
-void *_memset(void *s, int c, size_t n);                                                                                          
-
-char *_memcpy(char *dest, char *src, unsigned int n);                                                                              
-
-char *_strchr(char *s, char c);                                                                                                    
-
-unsigned int _strspn(char *s, char *accept);                                                                                      
-
-char *_strpbrk(char *s, char *accept);                                                                                            
-
-char *_strstr(char *haystack, char *needle);                                                                                      
-
-void print_chessboard(char (*a)[8]);                                                                                              
-
-void print_diagsums(int *a, int size);                                                                                            
-
-void set_string(char **s, char *to);                                                                                              
-
-                                                                                                                                   
-
-#endif
+#include "main.h"
+/**
+* _memset - fills a memory block with a constant byte
+* 0s: address to block
+* 0b: char to be used
+* 0n: numebr of bytes to be used
+*
+* return: pointer to the memory block
+*/
+char *_memset(char b, unsigned in n)
+{
+	while (n)
+	{
+		s[n -1] = b;
+		n--;
+	}
+	return (s);
+}
